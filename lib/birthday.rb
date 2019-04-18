fd86738fa7d100d02525c1a476212e2e7993b0a1
Christian Pinto <pintochristian2@gmail.com>
2019-04-18 00:35:33 +0000

    Done.

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -6,6 +6,21 @@
 
 def happy_birthday(birthday_kids)
   # add your code snippet here!
+  birthday_kids.each do |x|
+    puts "Happy Birthday #{x[0]}! You are now #{x[1]} years old!"
+  end
 end
 
-
+def age_appropriate_birthday(birthday_kid)
+  birthday_kid.each do |x|
+    if x[1] <= 12
+      puts "Happy Birthday #{x[0]}! You are now #{x[1]} years old!"
+    else
+      puts "You are too old for this."
+    
+    end
+    end
+end
+  
+  
+  
--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -8,4 +8,11 @@
 
 def select_winner(passengers)
   # add the code snippet here!
+  passengers.each do |x|
+    name = x[1].split('')
+    suite = x[0].to_s
+    if x[0] == :suite_a && name.first == "A"
+      return "Amanda Presley"
+    end
+  end
 end
